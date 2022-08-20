3d755de9ab857e733b9d95e74ac00fd53332239d
GoodDonkey <president20500@gmail.com>
2022-08-20 19:02:04 +0900

    initial commit

---
+++ src/main/java/com/biom/biombackend/BiomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.biom.biombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiomBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BiomBackendApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/biom/biombackend/BiomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biom.biombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiomBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
