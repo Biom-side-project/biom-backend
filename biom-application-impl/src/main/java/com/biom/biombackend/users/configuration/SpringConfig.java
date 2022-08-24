0ab92917d28d102d5ca5045a4b579c5dad9405ea
GoodDonkey <president20500@gmail.com>
2022-08-24 15:44:40 +0900

    [T-17] fix: missing dependency config
    SpringBootApplication 이 중복되었을 때 문제를 해결함.
    SpringBootApplication을 서브 모듈에서 제외하고
    SpringBootConfiguration 이 없는 문제를 @EnableAutoConfiguration 을 달아
    설정해주고, 없는 빈은 만들어 넣어주는 방법을 사용.

--- biom-application-impl/src/main/java/com/biom/biombackend/users/configuration/SpringConfig.java
+++ biom-application-impl/src/main/java/com/biom/biombackend/users/configuration/SpringConfig.java
@@ -9,8 +9,5 @@ import org.springframework.web.client.RestTemplate;
 @PropertySource(value = "classpath:env.${spring.config.activate.on-profile}.properties") // spring.profiles.active 는 주입시점이 늦다.
 public class SpringConfig {
     
-    @Bean
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+    @Bean RestTemplate restTemplate() { return new RestTemplate(); }
 }
