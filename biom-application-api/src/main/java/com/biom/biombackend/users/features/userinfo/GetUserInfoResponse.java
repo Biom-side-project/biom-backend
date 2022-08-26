17861a2c7bd61cccc2e1691ffd45bc69bbc321f4
GoodDonkey <president20500@gmail.com>
2022-08-26 17:56:03 +0900

    [T-22] feat: get user info

---
+++ biom-application-api/src/main/java/com/biom/biombackend/users/features/userinfo/GetUserInfoResponse.java
@@ -0,0 +1,13 @@
+package com.biom.biombackend.users.features.userinfo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetUserInfoResponse {
+    private String email;
+    private String username;
+    private String pictureUri;
+    private String nickname;
+}
---
+++ biom-application-impl/src/main/java/com/biom/biombackend/users/features/userinfo/DefaultUserInfoService.java
@@ -0,0 +1,33 @@
+package com.biom.biombackend.users.features.userinfo;
+
+import com.biom.biombackend.users.data.BiomUser;
+import com.biom.biombackend.users.data.BiomUserRepository;
+import com.biom.biombackend.users.excepions.ExceptionWithStatusCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+class DefaultUserInfoService implements UserService{
+    
+    private final BiomUserRepository userRepository;
+    
+    
+    @Override
+    public GetUserInfoResponse handle(GetUserInfo command) {
+        log.debug("handling command: {}", command);
+        Long userId = command.getUserId();
+        BiomUser user = userRepository.findById(userId)
+                                          .orElseThrow(() -> new ExceptionWithStatusCode("존재하지 않는 유저입니다.", 404));
+        GetUserInfoResponse response = createGetUserInfoResponse(user);
+        log.debug("created response: {}", response);
+        return response;
+    }
+    
+    private GetUserInfoResponse createGetUserInfoResponse(BiomUser user) {
+        return GetUserInfoResponse.builder().username(user.getUsername()).email(user.getEmail())
+                                  .pictureUri(user.getPictureUri()).nickname("").build();
+    }
+}
