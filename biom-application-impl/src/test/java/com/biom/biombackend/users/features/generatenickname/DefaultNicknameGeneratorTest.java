b2ccd0f2e55b8a8fd759122775ad80e5a4ee8321
GoodDonkey <president20500@gmail.com>
2022-08-26 19:49:30 +0900

    [T-22] chore: deleted SuccessResponseBody result field

--- biom-application-impl/src/test/java/com/biom/biombackend/users/features/generatenickname/DefaultNicknameGeneratorTest.java
+++ biom-application-impl/src/test/java/com/biom/biombackend/users/features/generatenickname/DefaultNicknameGeneratorTest.java
@@ -1,34 +1,16 @@
 package com.biom.biombackend.users.features.generatenickname;
 
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
-import java.util.Locale;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultNicknameGeneratorTest {
     private Integer howMany;
     private DefaultNicknameGenerator generator = new DefaultNicknameGenerator();
-    
-    @RepeatedTest(1)
-    @DisplayName("test01")
-    void test01() {
-        // given
-        Faker faker = new Faker(new Locale("ko"));
-        HashSet<String> names = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
-            String hex = faker.random().hex();
-            String name = faker.dragonBall().character();
-            names.add(name);
-            System.out.println("hex = " + hex);
-            System.out.println("name = " + name);
-        }
-    
-        howMany = names.size();
-        assertThat(names.size()).isEqualTo(1000);
-    }
     
     @Test
     @DisplayName("randomNickname")
