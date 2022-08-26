f5a7e144baac619d2f85d906815de545703a7422
GoodDonkey <president20500@gmail.com>
2022-08-26 12:35:39 +0900

    [T-10] feat: Created Date with Auditing listener

--- biom-application-impl/src/main/java/com/biom/biombackend/biom/data/BiomBaseEntity.java
+++ biom-application-impl/src/main/java/com/biom/biombackend/biom/data/BiomBaseEntity.java
@@ -5,16 +5,20 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BiomBaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
+    
     @LastModifiedDate
     private LocalDateTime lastModifiedAt;
 }
